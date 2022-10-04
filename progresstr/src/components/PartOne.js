580a4de324ffb32e64e9122d0d54159ff04ad88e
AnnModarageVaas <ann.s.dinali@gmail.com>
2022-10-05 10:50:05 +1300

    Fixed the bug ./components instead ./PartOne and ./Parttwo

--- progresstr/src/components/PartOne.js
+++ progresstr/src/components/PartOne.js
@@ -5,7 +5,7 @@ import './PartOne.css'
 export default function PartOne() {
   return (
     <div>
-      <h1 className='BeginnerCourse'>BEGINNER COURSE</h1>
+      <div className="BoxOne"></div>
 
     </div>
   )
---
+++ progresstr/src/components/ProgressTracker.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react'
+
+import PartOne from './PartOne'
+import PartTwo from './PartTwo'
+
+export default function ProgressTracker() {
+  return (
+    <div>
+     
+     <PartOne/>
+     <PartTwo/>
+     
+    </div>
+  )
+}
