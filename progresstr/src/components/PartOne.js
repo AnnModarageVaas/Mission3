5dd76a5f66f513d1e8d5ae8c00bd05e1d8c09036
AnnModarageVaas <ann.s.dinali@gmail.com>
2022-10-07 14:08:21 +1300

    added css to levels array

--- progresstr/src/components/PartOne.js
+++ progresstr/src/components/PartOne.js
@@ -19,7 +19,7 @@ export default function PartOne(props) {
          
             <div className='studentRecoContainer'>
             <StudentReco student='AIDEN ANDREWS'/>
-            <StudentReco levels={['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15']}/>
+            <StudentReco levels={[`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`10`,`11`,`12`,`13`,`14`,`15`]}/>
             
             <StudentReco student='COUTNEY BRISTOL'/>
             <StudentReco levels={['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15']}/>
